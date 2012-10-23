704bbdaf3618f4e73086faa580582aea53cd62b9
Mark Woan <markwoan@gmail.com>
2012-10-23 08:13:58 +0100

    Added assembly info

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("shimcacheparser")]
+[assembly: AssemblyDescription("Windows SHIM cache parser...")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("woanware")]
+[assembly: AssemblyProduct("shimcacheparser")]
+[assembly: AssemblyCopyright("Copyright © woanware 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4c87b437-f178-4820-bf9e-ae2cd3c96c9f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
